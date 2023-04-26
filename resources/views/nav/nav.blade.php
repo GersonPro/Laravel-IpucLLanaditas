5f370c0ce15f54072cd0f7f837a8b6966939464b
ingrid larfo <gers0nl4r6o2001@gmail.com>
2023-04-26 00:11:19 -0500

    nueva actualizacio

---
+++ resources/views/nav/nav.blade.php
@@ -0,0 +1,36 @@
+<nav class="bg-white shadow opacity-50 blur-sm">
+  <div class="container mx-auto px-6 py-4">
+    <div class="flex items-center justify-between">
+      <div class="flex items-center">
+        <a class="text-gray-800 text-xl font-bold md:text-2xl hover:text-gray-700 " href="#">
+        <?php
+        include 'images/IPUC Llanaditas Amarillo y azul.svg'
+        ?>
+         
+    </a>
+ 
+      </div>
+ 
+      <div class="hidden md:block">
+        <a class="text-gray-800 hover:text-gray-600 mx-4 " href="#">Membresia</a>
+        <a class="text-gray-800 hover:text-gray-600 mx-4" href="#">Contaduria</a>
+        <a class="text-gray-800 hover:text-gray-600" href="#">Directivas</a>
+      </div>
+ 
+      <div class="md:hidden flex">
+        <button type="button" class="text-gray-500 hover:text-gray-600 focus:outline-none focus:text-gray-600" aria-label="toggle menu">
+          <svg viewBox="0 0 24 24" class="h-6 w-6 fill-current">
+            <path fill-rule="evenodd" d="M3 5h18a1 1 0 110 2H3a1 1 0 110-2zm0 6h18a1 1 0 110 2H3a1 1 0 110-2zm0 6h18a1 1 0 110 2H3a1 1 0 110-2z"></path>
+          </svg>
+        </button>
+      </div>
+    </div>
+ 
+    <div class="md:hidden">
+      <div class="px-2 pt-2 pb-3 space-y-1 sm:px-3">
+      <a class="text-gray-800 hover:text-gray-600 mx-4 " href="#">Membresia</a>
+        <a class="text-gray-800 hover:text-gray-600 mx-4" href="#">Contaduria</a>
+        <a class="text-gray-800 hover:text-gray-600" href="#">Directivas</a>
+      </div>
+ 
+</nav>
