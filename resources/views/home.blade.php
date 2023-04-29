0094f67e11fd27b76daefb0ccb54545d8e4d8b30
ingrid larfo <gers0nl4r6o2001@gmail.com>
2023-04-29 08:46:39 -0500

    agregue los bas, la la tabla y una seccion con js

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,11 +5,16 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <link rel="stylesheet" href="resources/css/app.css">
   @vite('resources/css/app.css')
+  <script src="https://cdn.tailwindcss.com"></script>
+  <script src="https://right-distinct.tailwindui.com/script.js" data-site="SOXJJCZQ" data-auto="false" defer=""></script>
+  <script src="https://cdn.paddle.com/paddle/paddle.js"></script>
+  <script type="module" src="https://tailwindui.com/build/assets/app-9780190a.js"></script>
+
 </head>
 <body class="bg-gradient-to-r from-gray-800 via-blue-900 to-gray-800">
 
 @include('nav.nav')
-<div class="container mx-auto px-4 my-3 ">
+<div class="container mx-auto px-4 my-3 bg-white">
 <div class="container mx-auto">
   <div class="mt-10">
     <ul class="flex border-b">
@@ -31,62 +36,65 @@
     </ul>
   </div>
 </div>
-<section>
-  <header class="bg-white space-y-4 p-4 sm:px-8 sm:py-6 lg:p-4 xl:px-8 xl:py-6">
-    <div class="flex items-center justify-between">
-      <h2 class="font-semibold text-slate-900">Projects</h2>
-      <a href="/new" class="hover:bg-blue-400 group flex items-center rounded-md bg-blue-500 text-white text-sm font-medium pl-2 pr-3 py-2 shadow-sm">
-        <svg width="20" height="20" fill="currentColor" class="mr-2" aria-hidden="true">
-          <path d="M10 5a1 1 0 0 1 1 1v3h3a1 1 0 1 1 0 2h-3v3a1 1 0 1 1-2 0v-3H6a1 1 0 1 1 0-2h3V6a1 1 0 0 1 1-1Z" />
-        </svg>
-        New
-      </a>
-    </div>
-    <form class="group relative">
-      <svg width="20" height="20" fill="currentColor" class="absolute left-3 top-1/2 -mt-2.5 text-slate-400 pointer-events-none group-focus-within:text-blue-500" aria-hidden="true">
-        <path fill-rule="evenodd" clip-rule="evenodd" d="M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1110.89 3.476l4.817 4.817a1 1 0 01-1.414 1.414l-4.816-4.816A6 6 0 012 8z" />
-      </svg>
-      <input class="focus:ring-2 focus:ring-blue-500 focus:outline-none appearance-none w-full text-sm leading-6 text-slate-900 placeholder-slate-400 rounded-md py-2 pl-10 ring-1 ring-slate-200 shadow-sm" type="text" aria-label="Filter projects" placeholder="Filter projects...">
-    </form>
-  </header>
-  <ul class="bg-slate-50 p-4 sm:px-8 sm:pt-6 sm:pb-8 lg:p-4 xl:px-8 xl:pt-6 xl:pb-8 grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-1 xl:grid-cols-2 gap-4 text-sm leading-6">
-    <li x-for="project in projects">
-      <a :href="project.url" class="hover:bg-blue-500 hover:ring-blue-500 hover:shadow-md group rounded-md p-3 bg-white ring-1 ring-slate-200 shadow-sm">
-        <dl class="grid sm:block lg:grid xl:block grid-cols-2 grid-rows-2 items-center">
-          <div>
-            <dt class="sr-only">Title</dt>
-            <dd class="group-hover:text-white font-semibold text-slate-900">
-              {project.title}
-            </dd>
-          </div>
-          <div>
-            <dt class="sr-only">Category</dt>
-            <dd class="group-hover:text-blue-200">{project.category}</dd>
-          </div>
-          <div class="col-start-2 row-start-1 row-end-3 sm:mt-4 lg:mt-0 xl:mt-4">
-            <dt class="sr-only">Users</dt>
-            <dd x-for="user in project.users" class="flex justify-end sm:justify-start lg:justify-end xl:justify-start -space-x-1.5">
-              <img :src="user.avatar" :alt="user.name" class="w-6 h-6 rounded-full bg-slate-100 ring-2 ring-white" loading="lazy">
-            </dd>
-          </div>
-        </dl>
-      </a>
-    </li>
-    <li class="flex">
-      <a href="/new" class="hover:border-blue-500 hover:border-solid hover:bg-white hover:text-blue-500 group w-full flex flex-col items-center justify-center rounded-md border-2 border-dashed border-slate-300 text-sm leading-6 text-slate-900 font-medium py-3">
-        <svg class="group-hover:text-blue-500 mb-1 text-slate-400" width="20" height="20" fill="currentColor" aria-hidden="true">
-          <path d="M10 5a1 1 0 0 1 1 1v3h3a1 1 0 1 1 0 2h-3v3a1 1 0 1 1-2 0v-3H6a1 1 0 1 1 0-2h3V6a1 1 0 0 1 1-1Z" />
-        </svg>
-        New project
-      </a>
-    </li>
-  </ul>
-</section>
+<div class="mt-4 pb-4">
+        <div class="tab1 tab-content active">
+        <table class="border-collapse table-fixed w-full text-sm">
+      <thead class="bg-white   h-3">
+        <tr>
+          <th class="border-b dark:border-slate-900 font-medium p-4 pl-8 pt-0 pb-3 text-slate-400 dark:text-slate-900 text-left"><b>#</b></th>
+          <th class="border-b dark:border-slate-900 font-medium p-4 pt-0 pb-3 text-slate-400 dark:text-slate-900 text-left">Nombres</th>
+          <th class="border-b dark:border-slate-900 font-medium p-4 pr-8 pt-0 pb-3 text-slate-400 dark:text-slate-900 text-left">Apellidos</th>
+          <th class="border-b dark:border-slate-900 font-medium p-4 pr-8 pt-0 pb-3 text-slate-400 dark:text-slate-900 text-left">Edad</th>
+          <th class="border-b dark:border-slate-900 font-medium p-4 pr-8 pt-0 pb-3 text-slate-400 dark:text-slate-900 text-left">Bautizado</th>
+        </tr> <th class="border-b dark:border-slate-900 font-medium p-4 pr-8 pt-0 pb-3 text-slate-400 dark:text-slate-900 text-left">Promesa</th>
+        <th class="border-b dark:border-slate-900 font-medium p-4 pr-8 pt-0 pb-3 text-slate-400 dark:text-slate-900 text-left">Feecha de bautizado</th>
+        <th class="border-b dark:border-slate-900 font-medium p-4 pr-8 pt-0 pb-3 text-slate-400 dark:text-slate-900 text-left">Fecha Rec. Esp Santo</th>
+      </thead>
+      <tbody class="bg-white dark:bg-slate-100">
+        <tr>
+          <td class="border-b border-slate-100 dark:border-slate-300 p-4 pl-8 text-slate-500 dark:text-slate-700">The Sliding Mr. Bones (Next Stop, Pottersville)</td>
+          <td class="border-b border-slate-100 dark:border-slate-300 p-4 text-slate-500 dark:text-slate-700">Malcolm Lockyer</td>
+          <td class="border-b border-slate-100 dark:border-slate-300 p-4 pr-8 text-slate-500 dark:text-slate-700">1961</td>
+        </tr>
+        <tr>
+          <td class="border-b border-slate-100 dark:border-slate-300 p-4 pl-8 text-slate-500 dark:text-slate-700">Witchy Woman</td>
+          <td class="border-b border-slate-100 dark:border-slate-300 p-4 text-slate-500 dark:text-slate-700">The Eagles</td>
+          <td class="border-b border-slate-100 dark:border-slate-300 p-4 pr-8 text-slate-500 dark:text-slate-700">1972</td>
+        </tr>
+        <tr>
+          <td class="border-b border-slate-200 dark:border-slate-300 p-4 pl-8 text-slate-500 dark:text-slate-700">Shining Star</td>
+          <td class="border-b border-slate-200 dark:border-slate-300 p-4 text-slate-500 dark:text-slate-700">Earth, Wind, and Fire</td>
+          <td class="border-b border-slate-200 dark:border-slate-300 p-4 pr-8 text-slate-500 dark:text-slate-700">1975</td>
+        </tr>
+      </tbody>
+    </table>
+        </div>
+
 
 </div>
-<div class="footer" class="fixed bottom-0 left-0 w-full bg-gray-800 text-white text-center p-4">
-@include('footer.footer')
 </div>
 
 </body>
+<script>
+// Obtener el botón y el menú
+const button = document.querySelector('.relative button');
+const menu = document.querySelector('.relative div');
+
+// Agregar un evento al botón
+button.addEventListener('click', () => {
+  // Cambiar el estado del atributo aria-expanded
+  const expanded = button.getAttribute('aria-expanded') === 'true' || false;
+  button.setAttribute('aria-expanded', !expanded);
+  
+  // Mostrar u ocultar el menú
+  if (expanded) {
+    menu.classList.add('opacity-0', 'translate-y-1');
+    menu.classList.remove('opacity-100', 'translate-y-0');
+  } else {
+    menu.classList.add('opacity-100', 'translate-y-0');
+    menu.classList.remove('opacity-0', 'translate-y-1');
+  }
+});
+
+</script>
 </html>
