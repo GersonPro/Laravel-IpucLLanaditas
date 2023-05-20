fb7eb4e68a729782dbc780a64d0d875c3e2caa71
ingrid larfo <gers0nl4r6o2001@gmail.com>
2023-05-20 12:32:12 -0500

    Completed merge

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -16,6 +16,14 @@
   <script src="https://right-distinct.tailwindui.com/script.js" data-site="SOXJJCZQ" data-auto="false" defer=""></script>
   <script src="https://cdn.paddle.com/paddle/paddle.js"></script>
   <script type="module" src="https://tailwindui.com/build/assets/app-9780190a.js"></script>
+  <link href="https://cdnjs.cloudflare.com/ajax/libs/flowbite/1.6.5/flowbite.min.css" rel="stylesheet" />
+ <style>
+ body {
+    background-color: #F3F4F6 !important;
+}
+
+ </style>
+
 
 
     <title>{{ config('app.name', 'Laravel') }}</title>
@@ -32,18 +40,76 @@
     <div id="app">
         <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
             <div class="container">
-                <a class="navbar-brand" href="{{ url('/') }}">
-                    {{ config('app.name', 'Laravel') }}
+         
+                <a class="navbar-brand " href="{{ url('/') }}">
+                       
+            <?php
+        include 'images/IPUC Llanaditas Amarillo y azul.svg'
+        ?>
+        
                 </a>
-                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
-                    <span class="navbar-toggler-icon"></span>
-                </button>
+                          
+      <div class="hidden md:block  grid grid-cols-3 gap-4 font-semibold ">
+      <a class="text-gray-800 hover:text-gray-600 mx-4 hover:bg-slate-100 p-2 rounded " href="#">Inicio</a>
+        <a class="text-gray-800 hover:text-gray-600 mx-4 " href="#">Membresia</a>
+        <a class="text-gray-800 hover:text-gray-600 mx-4" href="#">Contaduria</a>
+        <a class="text-gray-800 hover:text-gray-600" href="#">Directivas</a>
 
+</div>
+
+       <div class="relative h-12 w-50 ...">
+       <button id="dropdownDefaultButton" data-dropdown-toggle="dropdown" class=" absolute inset-y-0 right-10 w-35 text-dark font-medium rounded-lg text-sm px-4 py-2.5 text-center inline-flex items-center   " type="button">
+       <!--avatar-->
+       <p class="text-gray-800 hover:text-gray-600 m-2 font-semibold" >{{ Auth::user()->name }}</p>
+       <img class="w-10 h-10 rounded-full" src="/images/Ellipse 3.png" alt="Rounded avatar">
+       <svg class="w-4 h-4 left-20 m-2" aria-hidden="true" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M19 9l-7 7-7-7"></path></svg></button>
+<!-- Dropdown menu -->
+<div id="dropdown" class="z-10 hidden bg-white divide-y divide-gray-100 rounded-lg shadow w-44 dark:bg-gray-700">
+    <ul class=" py-2 text-sm text-gray-700 dark:text-gray-700" aria-labelledby="dropdownDefaultButton">
+      <li>
+        <a href="#" class="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white ">Dashboard</a>
+      </li>
+      <li>
+        <a href="#" class="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Settings</a>
+      </li>
+      <li>
+        <a href="#" class="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Earnings</a>
+      </li>
+      <li>
+        
+        <form action="{{ route('logout') }}" method="POST" class="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">
+    
+    <button type="submit">Cerrar sesión</button>
+</form>
+      </li>
+    </ul>
+</div>
+       </div>
+
+
+          
+        <!-- aqui viene el animado-->
+      
+  <button type="button" class="inline-flex items-center gap-x-1 text-sm font-semibold leading-6 text-gray-900" aria-expanded="false">
+   
+
+         <!-- aqui viene el animado-->
+
+
+    <div class="md:hidden">
+      <div class="px-2 pt-2 pb-3 space-y-1 sm:px-3">
+      <a class="text-gray-800 hover:text-gray-600 mx-4 " href="#">Membresia</a>
+        <a class="text-gray-800 hover:text-gray-600 mx-4" href="#">Contaduria</a>
+        <a class="text-gray-800 hover:text-gray-600" href="#">Directivas</a>
+      </div>
+</div>
+</div>
+</div>
                 <div class="collapse navbar-collapse" id="navbarSupportedContent">
                     <!-- Left Side Of Navbar -->
                     <ul class="navbar-nav me-auto">
-
                     </ul>
+                    
 
                     <!-- Right Side Of Navbar -->
                     <ul class="navbar-nav ms-auto">
@@ -52,6 +118,7 @@
                             @if (Route::has('login'))
                                 <li class="nav-item">
                                     <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
+                                    
                                 </li>
                             @endif
 
@@ -83,162 +150,15 @@
                 </div>
             </div>
         </nav>
-        <nav class="bg-white shadow">
-  <div class="container mx-auto px-6 py-4">
-    <div class="flex items-center justify-between">
-      <div class="flex items-center">
-        <a class="text-gray-800 text-xl font-bold md:text-2xl hover:text-gray-700 " href="#">
-        <?php
-        include 'images/IPUC Llanaditas Amarillo y azul.svg'
-        ?>
-         
-    </a>
- 
-      </div>
- 
-      <div class="hidden md:block">
-        <a class="text-gray-800 hover:text-gray-600 mx-4 " href="#">Membresia</a>
-        <a class="text-gray-800 hover:text-gray-600 mx-4" href="#">Contaduria</a>
-        <a class="text-gray-800 hover:text-gray-600" href="#">Directivas</a>
-        <!-- aqui viene el animado-->
-        <div class="relative">
-  <button type="button" class="inline-flex items-center gap-x-1 text-sm font-semibold leading-6 text-gray-900" aria-expanded="false">
-    <span>Solutions</span>
-    <svg class="h-5 w-5" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
-      <path fill-rule="evenodd" d="M5.23 7.21a.75.75 0 011.06.02L10 11.168l3.71-3.938a.75.75 0 111.08 1.04l-4.25 4.5a.75.75 0 01-1.08 0l-4.25-4.5a.75.75 0 01.02-1.06z" clip-rule="evenodd" />
-    </svg>
-  </button>
-
-  <!--
-    Flyout menu, show/hide based on flyout menu state.
-
-    Entering: "transition ease-out duration-200"
-      From: "opacity-0 translate-y-1"
-      To: "opacity-100 translate-y-0"
-    Leaving: "transition ease-in duration-150"
-      From: "opacity-100 translate-y-0"
-      To: "opacity-0 translate-y-1"
-  -->
-  <div class="absolute left-1/2 z-10 mt-5 flex w-screen max-w-max -translate-x-1/2 px-4">
-    <div class="w-screen max-w-md flex-auto overflow-hidden rounded-3xl bg-white text-sm leading-6 shadow-lg ring-1 ring-gray-900/5">
-      <div class="p-4">
-        <div class="group relative flex gap-x-6 rounded-lg p-4 hover:bg-gray-50">
-          <div class="mt-1 flex h-11 w-11 flex-none items-center justify-center rounded-lg bg-gray-50 group-hover:bg-white">
-            <svg class="h-6 w-6 text-gray-600 group-hover:text-indigo-600" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" aria-hidden="true">
-              <path stroke-linecap="round" stroke-linejoin="round" d="M10.5 6a7.5 7.5 0 107.5 7.5h-7.5V6z" />
-              <path stroke-linecap="round" stroke-linejoin="round" d="M13.5 10.5H21A7.5 7.5 0 0013.5 3v7.5z" />
-            </svg>
-          </div>
-          <div>
-            <a href="#" class="font-semibold text-gray-900">
-              Analytics
-              <span class="absolute inset-0"></span>
-            </a>
-            <p class="mt-1 text-gray-600">Get a better understanding of your traffic</p>
-          </div>
-        </div>
-        <div class="group relative flex gap-x-6 rounded-lg p-4 hover:bg-gray-50">
-          <div class="mt-1 flex h-11 w-11 flex-none items-center justify-center rounded-lg bg-gray-50 group-hover:bg-white">
-            <svg class="h-6 w-6 text-gray-600 group-hover:text-indigo-600" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" aria-hidden="true">
-              <path stroke-linecap="round" stroke-linejoin="round" d="M15.042 21.672L13.684 16.6m0 0l-2.51 2.225.569-9.47 5.227 7.917-3.286-.672zM12 2.25V4.5m5.834.166l-1.591 1.591M20.25 10.5H18M7.757 14.743l-1.59 1.59M6 10.5H3.75m4.007-4.243l-1.59-1.59" />
-            </svg>
-          </div>
-          <div>
-            <a href="#" class="font-semibold text-gray-900">
-              Engagement
-              <span class="absolute inset-0"></span>
-            </a>
-            <p class="mt-1 text-gray-600">Speak directly to your customers</p>
-          </div>
-        </div>
-        <div class="group relative flex gap-x-6 rounded-lg p-4 hover:bg-gray-50">
-          <div class="mt-1 flex h-11 w-11 flex-none items-center justify-center rounded-lg bg-gray-50 group-hover:bg-white">
-            <svg class="h-6 w-6 text-gray-600 group-hover:text-indigo-600" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" aria-hidden="true">
-              <path stroke-linecap="round" stroke-linejoin="round" d="M7.864 4.243A7.5 7.5 0 0119.5 10.5c0 2.92-.556 5.709-1.568 8.268M5.742 6.364A7.465 7.465 0 004.5 10.5a7.464 7.464 0 01-1.15 3.993m1.989 3.559A11.209 11.209 0 008.25 10.5a3.75 3.75 0 117.5 0c0 .527-.021 1.049-.064 1.565M12 10.5a14.94 14.94 0 01-3.6 9.75m6.633-4.596a18.666 18.666 0 01-2.485 5.33" />
-            </svg>
-          </div>
-          <div>
-            <a href="#" class="font-semibold text-gray-900">
-              Security
-              <span class="absolute inset-0"></span>
-            </a>
-            <p class="mt-1 text-gray-600">Your customers&#039; data will be safe and secure</p>
-          </div>
-        </div>
-        <div class="group relative flex gap-x-6 rounded-lg p-4 hover:bg-gray-50">
-          <div class="mt-1 flex h-11 w-11 flex-none items-center justify-center rounded-lg bg-gray-50 group-hover:bg-white">
-            <svg class="h-6 w-6 text-gray-600 group-hover:text-indigo-600" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" aria-hidden="true">
-              <path stroke-linecap="round" stroke-linejoin="round" d="M13.5 16.875h3.375m0 0h3.375m-3.375 0V13.5m0 3.375v3.375M6 10.5h2.25a2.25 2.25 0 002.25-2.25V6a2.25 2.25 0 00-2.25-2.25H6A2.25 2.25 0 003.75 6v2.25A2.25 2.25 0 006 10.5zm0 9.75h2.25A2.25 2.25 0 0010.5 18v-2.25a2.25 2.25 0 00-2.25-2.25H6a2.25 2.25 0 00-2.25 2.25V18A2.25 2.25 0 006 20.25zm9.75-9.75H18a2.25 2.25 0 002.25-2.25V6A2.25 2.25 0 0018 3.75h-2.25A2.25 2.25 0 0013.5 6v2.25a2.25 2.25 0 002.25 2.25z" />
-            </svg>
-          </div>
-          <div>
-            <a href="#" class="font-semibold text-gray-900">
-              Integrations
-              <span class="absolute inset-0"></span>
-            </a>
-            <p class="mt-1 text-gray-600">Connect with third-party tools</p>
-          </div>
-        </div>
-        <div class="group relative flex gap-x-6 rounded-lg p-4 hover:bg-gray-50">
-          <div class="mt-1 flex h-11 w-11 flex-none items-center justify-center rounded-lg bg-gray-50 group-hover:bg-white">
-            <svg class="h-6 w-6 text-gray-600 group-hover:text-indigo-600" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" aria-hidden="true">
-              <path stroke-linecap="round" stroke-linejoin="round" d="M16.023 9.348h4.992v-.001M2.985 19.644v-4.992m0 0h4.992m-4.993 0l3.181 3.183a8.25 8.25 0 0013.803-3.7M4.031 9.865a8.25 8.25 0 0113.803-3.7l3.181 3.182m0-4.991v4.99" />
-            </svg>
-          </div>
-          <div>
-            <a href="#" class="font-semibold text-gray-900">
-              Automations
-              <span class="absolute inset-0"></span>
-            </a>
-            <p class="mt-1 text-gray-600">Build strategic funnels that will convert</p>
-          </div>
-        </div>
-      </div>
-      <div class="grid grid-cols-2 divide-x divide-gray-900/5 bg-gray-50">
-        <a href="#" class="flex items-center justify-center gap-x-2.5 p-3 font-semibold text-gray-900 hover:bg-gray-100">
-          <svg class="h-5 w-5 flex-none text-gray-400" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
-            <path fill-rule="evenodd" d="M2 10a8 8 0 1116 0 8 8 0 01-16 0zm6.39-2.908a.75.75 0 01.766.027l3.5 2.25a.75.75 0 010 1.262l-3.5 2.25A.75.75 0 018 12.25v-4.5a.75.75 0 01.39-.658z" clip-rule="evenodd" />
-          </svg>
-          Watch demo
-        </a>
-        <a href="#" class="flex items-center justify-center gap-x-2.5 p-3 font-semibold text-gray-900 hover:bg-gray-100">
-          <svg class="h-5 w-5 flex-none text-gray-400" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
-            <path fill-rule="evenodd" d="M2 3.5A1.5 1.5 0 013.5 2h1.148a1.5 1.5 0 011.465 1.175l.716 3.223a1.5 1.5 0 01-1.052 1.767l-.933.267c-.41.117-.643.555-.48.95a11.542 11.542 0 006.254 6.254c.395.163.833-.07.95-.48l.267-.933a1.5 1.5 0 011.767-1.052l3.223.716A1.5 1.5 0 0118 15.352V16.5a1.5 1.5 0 01-1.5 1.5H15c-1.149 0-2.263-.15-3.326-.43A13.022 13.022 0 012.43 8.326 13.019 13.019 0 012 5V3.5z" clip-rule="evenodd" />
-          </svg>
-          Contact sales
-        </a>
-      </div>
-    </div>
-  </div>
-</div>
-
-         <!-- aqui viene el animado-->
-
-      </div>
- 
-      <div class="md:hidden flex">
-        <button type="button" class="text-gray-500 hover:text-gray-600 focus:outline-none focus:text-gray-600" aria-label="toggle menu">
-          <svg viewBox="0 0 24 24" class="h-6 w-6 fill-current">
-            <path fill-rule="evenodd" d="M3 5h18a1 1 0 110 2H3a1 1 0 110-2zm0 6h18a1 1 0 110 2H3a1 1 0 110-2zm0 6h18a1 1 0 110 2H3a1 1 0 110-2z"></path>
-          </svg>
-        </button>
-      </div>
-    </div>
- 
-    <div class="md:hidden">
-      <div class="px-2 pt-2 pb-3 space-y-1 sm:px-3">
-      <a class="text-gray-800 hover:text-gray-600 mx-4 " href="#">Membresia</a>
-        <a class="text-gray-800 hover:text-gray-600 mx-4" href="#">Contaduria</a>
-        <a class="text-gray-800 hover:text-gray-600" href="#">Directivas</a>
-      </div>
- 
-</nav>
+       
 
             <main class="py-4">
                 @yield('content')
             </main>
         </div>
-       
+        <script src="../path/to/flowbite/dist/flowbite.min.js"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/flowbite/1.6.5/flowbite.min.js"></script>
+
 </body>
 <script>
     // Obtener el botón y el menú
@@ -262,4 +182,5 @@
     });
 
     </script>
+    
 </html>
